0dad0d78296b18572901b7e0aef06a923c5e9d24
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-17 09:13:18 -0400

    drill into the iframe

--- src/components/demos/CardForm.cy-spec.js
+++ src/components/demos/CardForm.cy-spec.js
@@ -21,23 +21,50 @@ const style = `
 
 describe('Card', () => {
   it('looks pretty', () => {
-    mount(<Elements stripe={stripePromise}><CardForm /></Elements>, {
-      style
-    })
+    mount(
+      <Elements stripe={stripePromise}>
+        <CardForm />
+      </Elements>,
+      {
+        style
+      }
+    )
+
+    const cc = '1111 2222 3333 4444'
+    const month = '01'
+    const year = '30'
+    const cvc = '123'
 
     // credit card form is inside an iframe
     // so we will need to take care when accessing it
-    // cy.get('input[name=cardnumber]')
+    const iframeSelector = '[title="Secure payment input frame"]'
+    cy.frameLoaded(iframeSelector)
+    cy.iframe(iframeSelector).find('input[name=cardnumber]')
+      .type(`${cc} ${month} ${year}`)
+
+    cy.iframe(iframeSelector).find('input[name=cvc]')
+      .type(cvc)
 
     // wait for stripe to load
-    cy.window().should('have.property', 'stripe')
+    // cy.window().should('have.property', 'stripe')
     // let's spy!
-    .then(stripe => {
+    // .then(stripe => {
+      // TODO figure out the getter that prevents stubbing
       // cy.spy(stripe, 'createPaymentMethod').as('createPaymentMethod')
-    })
+    // })
 
-    // the Pay button is outside the iframe
+    // TODO look up how we set up XHR interception in component tests
+    // then copy the enhanced object into the iframe (is it even necessary?)
+    cy.server()
+    cy.route('POST', 'https://api.stripe.com/v1/payment_methods', 'fx:incorrect-number').as('pay')
 
+    cy.get(iframeSelector).its('0.contentWindow')
+      .then(win => {
+        win.XMLHttpRequest = XMLHttpRequest
+        debugger
+      })
+
+    // the Pay button is outside the iframe, so just click it
     cy.contains('button', 'Pay').click()
   })
 })
